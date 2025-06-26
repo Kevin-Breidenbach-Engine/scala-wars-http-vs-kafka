60d4b5973415978708c5f04ec8f188c398f53cf8
Kevin Breidenbach <kbreidenbach@moneylion.com>
2025-06-25 22:00:10 -0500

    Main class implementation for kafka service

--- modules/http-service/src/main/scala/com/moneylion/scalawars/httpservice/Main.scala
+++ modules/http-service/src/main/scala/com/moneylion/scalawars/httpservice/Main.scala
@@ -1,10 +1,15 @@
 package com.moneylion.scalawars.httpservice
 
+import com.moneylion.scalawars.httpservice.config.Config
+
 import cats.effect.ExitCode
 import cats.effect.IO
 import cats.effect.IOApp
+import cats.syntax.all.given
 
 object Main extends IOApp {
-
-  override def run(args: List[String]): IO[ExitCode] = IO(ExitCode.Success)
+  override def run(args: List[String]): IO[ExitCode] =
+    Config.make[IO].appConfig.load.flatMap { _ =>
+      ExitCode.Success.pure[IO]
+    }
 }
